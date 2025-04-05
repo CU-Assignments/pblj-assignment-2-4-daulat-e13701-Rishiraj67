505ed269d51d0953001b7bbc663281d8e45dcfac
Rishiraj67 <138872831+Rishiraj67@users.noreply.github.com>
2025-04-05 16:07:54 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+import controller.EmployeeController;
+
+public class Main {
+    public static void main(String[] args) {
+        EmployeeController controller = new EmployeeController();
+        controller.showAllEmployees();
+    }
+}
